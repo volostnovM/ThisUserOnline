47f703c7ff92ed92b590875eee4728ba86195598
Volostnov Maksim <volostnov@tele2med.ru>
2023-05-05 20:24:26 +0300

    Исправление замечаний

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println(agoToText(100000))
+    println(agoToText(20254))
 
 }
 
@@ -7,37 +7,27 @@ fun agoToText(secondsSinceLogin: Int): String = when (secondsSinceLogin) {
     in 1..60 -> "был(а) в сети только что"
     in 61..60 * 60 -> "был(а) в сети ${calculateMinuts(secondsSinceLogin)}"
     in 60 * 60 + 1..24 * 60 * 60 -> "был(а) в сети ${calculateHours(secondsSinceLogin)}"
-    in 24 * 60 * 60 + 1 .. 24 * 60 * 60 * 2 -> "был(а) в сети вчера"
-    in 24 * 60 * 60 * 2 + 1 .. 24 * 60 * 60 * 3 ->  "был(а) в сети позавчера"
+    in 24 * 60 * 60 + 1..24 * 60 * 60 * 2 -> "был(а) в сети вчера"
+    in 24 * 60 * 60 * 2 + 1..24 * 60 * 60 * 3 -> "был(а) в сети позавчера"
     else -> "был(а) в сети давно"
 }
 
 fun calculateMinuts(seconds: Int): String {
     var minuts: Int = seconds / 60
-    when(minuts) {
-        1 -> return "$minuts минуту назад"
-        in 2 .. 4 -> return "$minuts минуты назад"
-        in 6 .. 9 -> return "$minuts минут назад"
-        5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55 -> return "$minuts минут назад"
-        11, 21, 31, 41, 51 -> return "$minuts минут назад"
-        in 12 .. 19 -> return "$minuts минут назад"
-        in 22 .. 29 -> return "$minuts минут назад"
-        in 32 .. 39 -> return "$minuts минут назад"
-        in 42 .. 49 -> return "$minuts минут назад"
-        in 52 .. 59 -> return "$minuts минут назад"
-        else -> return "недавно"
+
+    return when {
+        minuts % 10 == 1 && minuts != 11 -> "$minuts минуту назад"
+        minuts % 10 in 2..4 && minuts !in 12..14 -> "$minuts минуты назад"
+        else -> "$minuts минут назад"
     }
 }
 
 fun calculateHours(seconds: Int): String {
     var hours: Int = (seconds / 60) / 60
-    when(hours) {
-        1 -> return "$hours час назад"
-        in 2 .. 4 -> return "$hours часа назад"
-        in 6 .. 19 -> return "$hours часов назад"
-        5, 10, 15, 20 -> return "$hours часов назад"
-        21 -> return "$hours час назад"
-        in 22 .. 24 -> return "$hours часа назад"
-        else -> return "недавно"
+
+    return when {
+        hours % 10 == 1 && hours != 11 -> "$hours час назад"
+        hours % 10 in 2..4 && hours !in 12..14 -> "$hours часа назад"
+        else -> "$hours часов назад"
     }
 }
